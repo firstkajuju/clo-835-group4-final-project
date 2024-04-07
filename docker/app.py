ca4419af102728c56aad5ed7074cc234b926ce9e
M-Rasoulzadeh <ec2-user@ip-172-31-18-49.ec2.internal>
2024-04-07 01:27:18 +0000

    fix app and msql

--- docker/app.py
+++ docker/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from pymysql import connections
 import os
 import random
 import argparse
-
-
+import boto3
+import botocore
 app = Flask(__name__)
 
 DBHOST = os.environ.get("DBHOST") or "localhost"
@@ -13,15 +13,20 @@ DBPWD = os.environ.get("DBPWD") or "passwors"
 DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
+BACKGROUND_IMAGE = os.environ.get("BACKGROUND_IMAGE") or "Invalid Image been passed"
+GROUP_NAME = os.environ.get('GROUP_NAME') or "GROUP4"
+
+
+print('GROUP_NAME:', GROUP_NAME)
+
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
     host= DBHOST,
     port=DBPORT,
     user= DBUSER,
-    password= DBPWD, 
+    password= DBPWD,
     db= DATABASE
-    
 )
 output = {}
 table = 'employee';
@@ -44,74 +49,118 @@ SUPPORTED_COLORS = ",".join(color_codes.keys())
 # Generate a random color
 COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
 
-
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', color=color_codes[COLOR])
+    print('show me the background image url',BACKGROUND_IMAGE)
+    image_url = url_for('static', filename='background_image.png')
+    group_name = os.environ.get('GROUP_NAME', 'DefaultGroup')
+    return render_template('addemp.html', background_image = image_url, group_name = group_name)
+@app.route("/download", methods=['GET','POST'])
+
+def download(image_url):
+   try:
+         bucket = image_url.split('//')[1].split('.')[0]
+         object_name = '/'.join(image_url.split('//')[1].split('/')[1:])
+         print(bucket)  # prints 'finalbrojectmoishtor'
+         print(object_name)  # prints'wallpaperflare.com_wallpaper.jpg' 
+         print("Background Image Location --->" + image_url) # Added for Logging of Background Image Path
+         s3 = boto3.resource('s3')
+         output_dir = "static"
+         if not os.path.exists(output_dir):
+                 os.makedirs(output_dir)
+         output = os.path.join(output_dir, "background_image.png")
+         s3.Bucket(bucket).download_file(object_name, output)
+
+         return output
+
+   except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == "404":
+                    print("The object does not exist.")
+                else:
+                    raise
+
+
+
+
 
 @app.route("/about", methods=['GET','POST'])
 def about():
-    return render_template('about.html', color=color_codes[COLOR])
+    group_name = os.environ.get('GROUP_NAME', 'DefaultGroup')
+    image_url = url_for('static', filename='background_image.png')
+    return render_template('about.html', background_image = image_url, group_name = group_name)
     
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
+    # Capture form data
     emp_id = request.form['emp_id']
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     primary_skill = request.form['primary_skill']
     location = request.form['location']
 
-  
+    # Prepare SQL for inserting new employee
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     try:
-        
-        cursor.execute(insert_sql,(emp_id, first_name, last_name, primary_skill, location))
+        # Attempt to insert new employee into the database
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, primary_skill, location))
         db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-
+        emp_name = f"{first_name} {last_name}"
+    except Exception as e:
+        print(e)
+        emp_name = "Error adding employee"
     finally:
         cursor.close()
 
-    print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
+   
+    image_url = url_for('static', filename='background_image.png')
+    group_name = os.environ.get('GROUP_NAME', 'DefaultGroup')
 
+   
+    return render_template('addempoutput.html', background_image=image_url, group_name=group_name, name=emp_name)
+    
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
+    image_url = url_for('static', filename='background_image.png')
+    group_name = os.environ.get('GROUP_NAME', 'DefaultGroup')
+    return render_template("getemp.html", background_image = image_url, group_name = group_name)
 
 
-@app.route("/fetchdata", methods=['GET','POST'])
+@app.route("/fetchdata", methods=['POST'])  
 def FetchData():
-    emp_id = request.form['emp_id']
+    emp_id = request.form.get('emp_id')  
 
     output = {}
-    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location from employee where emp_id=%s"
+    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location from employee where emp_id = %s"
     cursor = db_conn.cursor()
 
     try:
-        cursor.execute(select_sql,(emp_id))
+        cursor.execute(select_sql, (emp_id,))
         result = cursor.fetchone()
-        
-        # Add No Employee found form
-        output["emp_id"] = result[0]
-        output["first_name"] = result[1]
-        output["last_name"] = result[2]
-        output["primary_skills"] = result[3]
-        output["location"] = result[4]
-        
+
+        if result:
+            output = {
+                "emp_id": result[0],
+                "first_name": result[1],
+                "last_name": result[2],
+                "primary_skills": result[3],
+                "location": result[4]
+            }
+        else:
+            output["error"] = "Employee not found"
     except Exception as e:
         print(e)
-
+        output["error"] = "Error fetching employee data"
     finally:
         cursor.close()
 
-    return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR])
+    image_url = url_for('static', filename='background_image.png')
+    group_name = os.environ.get('GROUP_NAME', 'DefaultGroup')
+    return render_template("getempoutput.html", background_image=image_url, group_name=group_name, **output)
 
 if __name__ == '__main__':
-    
+    download(BACKGROUND_IMAGE)
     # Check for Command Line Parameters for color
     parser = argparse.ArgumentParser()
     parser.add_argument('--color', required=False)
@@ -133,4 +182,4 @@ if __name__ == '__main__':
         print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
         exit(1)
 
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=81,debug=True)
